21ba5fa89e2c7a37a682002468e3a80993ca6c24
Jakub kub1x Podlaha <j.podlaha@gmail.com>
2017-11-01 15:58:38 +0100

    sigfocx_callback: changed TCP to HTTP server

--- sigfox_callback/index.js
+++ sigfox_callback/index.js
@@ -1,24 +1,23 @@
-require('dotenv').config();
 var net = require('net');
 var redisClient = require('redis').createClient();
 
-const REDIS_IOTBEESCALE_LIST = process.env.REDIS_IOTBEESCALE_LIST;
-var server = net.createServer(function(socket) {
-  console.log(`Client connected from: ${socket.remoteAddress}:${socket.remotePort}`);
-  socket.on('data', function(data) {
-    console.log(`${socket.remoteAddress}:${socket.remotePort} sent data: ${data}`);
-    // Expected data structure:
-    //weight::int:16 hive::int:16 voltage::uint:8 raw::int:16 diffmin::uint:8 diffmax::uint:8 scale::int:16
-    redisClient.rpush([REDIS_IOTBEESCALE_LIST, data]);
-    socket.end(function() {
-      console.log(`Connection to ${socket.remoteAddress}:${socket.remotePort} was ended.`);
-    });
-  });
-});
-
-
+require('dotenv').config();
 const HOST = process.env.HOST;
 const PORT = process.env.PORT;
+const REDIS_IOTBEESCALE_LIST = process.env.REDIS_IOTBEESCALE_LIST;
+
+var http = require('http');
+var server = http.createServer(function(req, res){
+    console.log(`Connection from: ${req.method} ${res.socket.remoteAddress}:${res.socket.remotePort}\n${JSON.stringify(req.headers, null, 2)}`);
+    req.on('data', function(data) {
+      console.log(`Got data from ${res.socket.remoteAddress}:${res.socket.remotePort}:\n` + data);
+
+      redisClient.rpush([REDIS_IOTBEESCALE_LIST, data]);
+
+      res.writeHead(200, ['Content-Type', 'text/plain']);
+      res.end('');
+    });
+});
 server.listen(PORT, HOST, function() {
-  console.log(`Server listening on ${HOST}:${PORT}`);
+  console.log(`Server listening on http://${HOST}:${PORT} now...`);
 });
