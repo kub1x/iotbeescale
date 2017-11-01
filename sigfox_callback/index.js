d0f7c97ba240eb0a1ff2872f32ef54c502752351
Jakub kub1x Podlaha <j.podlaha@gmail.com>
2017-11-01 14:07:19 +0100

    sigfocx_callback: first prototype for data analysis

--- sigfox_callback/index.js
+++ sigfox_callback/index.js
@@ -1,12 +1,17 @@
 var net = require('net');
 var redisClient = require('redis').createClient();
 
-const REDIS_IOTBEESCALE_LIST = 'iotbeescale';
+const REDIS_IOTBEESCALE_LIST = 'iotbeescale-init_test';
 
 var server = net.createServer(function(socket) {
   socket.on('data', function(data) {
+    //weight::int:16 hive::int:16 voltage::uint:8 raw::int:16 diffmin::uint:8 diffmax::uint:8 scale::int:16
     redisClient.rpush([REDIS_IOTBEESCALE_LIST, data]);
   });
 });
 
-server.listen(1337, '127.0.0.1');
+const IP = '127.0.0.1';
+const PORT = 13737;
+server.listen(PORT, IP, function() {
+  console.log(`Server listening on ${IP}:${PORT}`);
+});
