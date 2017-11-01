f45beb59f155bbce0c993db2426b9597fb102454
Jakub kub1x Podlaha <j.podlaha@gmail.com>
2017-11-01 14:20:37 +0100

    sigfocx_callback: add forever

--- sigfox_callback/index.js
+++ sigfox_callback/index.js
@@ -4,9 +4,18 @@ var redisClient = require('redis').createClient();
 const REDIS_IOTBEESCALE_LIST = 'iotbeescale-init_test';
 
 var server = net.createServer(function(socket) {
+
+  console.log(`Client connected from: ${socket.remoteAddress}`);
+
   socket.on('data', function(data) {
+
+    console.log(`${socket.remoteAddress} sent data: ${data}`);
+
+    // Expected data structure:
     //weight::int:16 hive::int:16 voltage::uint:8 raw::int:16 diffmin::uint:8 diffmax::uint:8 scale::int:16
+
     redisClient.rpush([REDIS_IOTBEESCALE_LIST, data]);
+
   });
 });
 
