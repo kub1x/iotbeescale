c7e47c294a2c854a09e783b001795b09f66994ee
Jakub kub1x Podlaha <j.podlaha@gmail.com>
2017-11-07 10:27:18 +0000

    sigfocx_callback: fix es manual_testing index url

--- sigfox_callback/test/send_json_array_to_es.py
+++ sigfox_callback/test/send_json_array_to_es.py
@@ -16,7 +16,7 @@ for f in data_loaded:
     str_ = json.dumps(f, indent=2, sort_keys=True, separators=(',', ': '), ensure_ascii=False)
     print("Sending: " + str_)
 
-    req = requests.post('http://localhost:9200/iotbeescale-manual_test', data=str_)
+    req = requests.post('http://localhost:9200/iotbeescale-manual_test/weight/', data=str_)
     print req.text
 
     time.sleep(5)
